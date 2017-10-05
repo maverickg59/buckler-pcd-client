eec60955356f26fd8319478c5f10a3ce67e8a63a
Christopher White <Maverick@Christophers-MacBook-Air.local>
2017-10-04 21:15:30 -0600

    changes to styling

--- client/public/app.js
+++ client/public/app.js
@@ -9,7 +9,68 @@ $(document).ready(function() {
     $('.append').empty()
   })
 })
+function processFirstNpi(data) {
+  var npi = data[0].number
+  var bizName = data[0].basic.name
+  var lastName = data[0].basic.last_name
+  var firstName = data[0].basic.first_name
+  var credential = data[0].taxonomies[0].desc
+  var cityState = data[0].addresses[0].city + ', ' + data[0].addresses[0].state
+  var taxZeroCode = data[0].taxonomies[0].code
+  var taxOneCode
+  var yes = data[0].hasOwnProperty('taxonomies[1]')
+  function taxOneExists() {
+    if (yes === true) {
+      taxOneCode = data[0].taxonomies[1].code
+    } else {
+      taxOneCode = 'N/A'
+    }
+  }
+  taxOneExists(yes)
+  var soleProp = data[0].basic.sole_proprietor
+  var orgSubPart
+  var sub = data[0].basic.hasOwnProperty('organizational_subpart')
+  function subExists() {
+    if (sub === true) {
+      orgSubPart = data[0].basic.organizational_subpart
+    } else {
+      orgSubPart = 'N/A'
+    }
+  }
+  subExists(sub)
 
+  $('.append').append(
+    `
+    <table style="width:100%">
+      <tr>
+        <th style="width:10%">Given NPI:</th>
+        <th style="width:10%">Bus Name:</th>
+        <th style="width:10%">Last:</th>
+        <th style="width:10%">First:</th>
+        <th style="width:10%">Credential:</th>
+        <th style="width:10%">City & State:</th>
+        <th style="width:10%">Taxonomy One:</th>
+        <th style="width:10%">Taxonomy Two:</th>
+        <th style="width:10%">Sole Prop:</th>
+        <th style="width:10%">Org Subpart:</th>
+      </tr>
+      <tr class="coloredFirst">
+        <td style="width:10%">${npi}</td>
+        <td style="width:10%">${bizName}</td>
+        <td style="width:10%">${lastName}</td>
+        <td style="width:10%">${firstName}</td>
+        <td style="width:10%">${credential}</td>
+        <td style="width:10%">${cityState}</td>
+        <td style="width:10%">${taxZeroCode}</td>
+        <td style="width:10%">${taxOneCode}</td>
+        <td style="width:10%">${soleProp}</td>
+        <td style="width:10%">${orgSubPart}</td>
+      </tr>
+    </table>
+    `
+  )
+  console.log(data)
+}
 function processNpi(data) {
   var npi = data[0].number
   var bizName = data[0].basic.name
@@ -41,38 +102,45 @@ function processNpi(data) {
   subExists(sub)
 
   $('.append').append(
-    `<stong><p class="provider-result">${'NPI: ' +
-      npi +
-      ' | Business Name: ' +
-      bizName +
-      ' | Last Name: ' +
-      lastName +
-      ' | First Name: ' +
-      firstName +
-      ' | Credentials: ' +
-      credential +
-      ' | City & State: ' +
-      cityState +
-      ' | Taxonomy Code One: ' +
-      taxZeroCode +
-      ' | Taxonomy Code Two: ' +
-      taxOneCode +
-      ' | Sole Proprietor: ' +
-      soleProp +
-      ' | Organizational Subpart: ' +
-      orgSubPart}</p></strong>`
+    `
+    <table style="width:100%">
+      <tr>
+        <th style="width:10%">NPI:</th>
+        <th style="width:10%">Bus Name:</th>
+        <th style="width:10%">Last:</th>
+        <th style="width:10%">First:</th>
+        <th style="width:10%">Credential:</th>
+        <th style="width:10%">City & State:</th>
+        <th style="width:10%">Taxonomy One:</th>
+        <th style="width:10%">Taxonomy Two:</th>
+        <th style="width:10%">Sole Prop:</th>
+        <th style="width:10%">Org Subpart:</th>
+      </tr>
+      <tr class="coloredSecond">
+        <td style="width:10%">${npi}</td>
+        <td style="width:10%">${bizName}</td>
+        <td style="width:10%">${lastName}</td>
+        <td style="width:10%">${firstName}</td>
+        <td style="width:10%">${credential}</td>
+        <td style="width:10%">${cityState}</td>
+        <td style="width:10%">${taxZeroCode}</td>
+        <td style="width:10%">${taxOneCode}</td>
+        <td style="width:10%">${soleProp}</td>
+        <td style="width:10%">${orgSubPart}</td>
+      </tr>
+    </table>
+    `
   )
   console.log(data)
 }
 function runSearch() {
   event.preventDefault()
   var searchInput = $('.search-box').val()
+  $.get('https://cors-anywhere.herokuapp.com/https://npiregistry.cms.hhs.gov/api/?number=' + searchInput, function({ results }) {
+    processFirstNpi(results)
+  })
 
-  $.get('http://localhost:3000/api/v1/new-similars/' + searchInput, function(data) {
-    console.log(data)
-    // var objLength = Object.keys(data).length
-    // var dataArray = Object.values(data)
-
+  $.get('https://cors-anywhere.herokuapp.com/https://buckler-pcd.herokuapp.com/api/v1/new-similars/' + searchInput, function(data) {
     data.map(row => {
       $.get('https://cors-anywhere.herokuapp.com/https://npiregistry.cms.hhs.gov/api/?number=' + row.similar, function({ results }) {
         processNpi(results)
